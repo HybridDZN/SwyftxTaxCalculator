34c8cd5ef4bf6340bfa1af74597b92d593aa1e83
HybridDZN <marius.scutea.00@gmail.com>
2022-07-08 10:46:30 +0930

    Create Transaction.py
    Should be used to create transaction objects for buy/sell events

---
+++ Transaction.py
@@ -0,0 +1,11 @@
+class Transaction:
+
+    name
+    value
+    event
+
+    def __init__(self, name, value, event):
+        self.name = name
+        self.value = value
+        self.event = event
+    
